3d0c3a4c6e56cf8208c8ad540798af1769a6fd36
marcmelis <marc.melis@hayanmind.com>
2021-01-14 19:52:44 +0100

    add initrevision functionality

--- src/treatment/ConsultationTerminal.java
+++ src/treatment/ConsultationTerminal.java
@@ -1,20 +1,36 @@
 package treatment;
 
+import data.HealthCardID;
 import exceptions.*;
+import medicalconsultation.MedicalPrescription;
+import services.HealthNationalService;
+import services.ScheduledVisitAgenda;
 
 import java.net.ConnectException;
 import java.util.Date;
 
 public class ConsultationTerminal {
+    private ScheduledVisitAgenda sva;
+    private HealthNationalService hns;
 
     public void initRevision()
             throws HealthCardException, NotValidePrescription, ConnectException {
+        HealthCardID hcID = sva.getHealthCardID();
+        MedicalPrescription medicalPrescription = hns.getePrescription(hcID);
+        System.out.println(medicalPrescription);
+    }
 
+    public void setScheduledVisitAgenda(ScheduledVisitAgenda sva){
+        this.sva = sva;
+    }
+
+    public void setHealthNationalService(HealthNationalService hns) {
+        this.hns = hns;
     }
 
     public void initPrescriptionEdition()
             throws AnyCurrentPrescriptionException, NotFinishedTreatmentException {
-
+        
     }
 
     public void searchForProducts(String keyWord)
